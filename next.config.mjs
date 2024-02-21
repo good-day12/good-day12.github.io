31d31d508ce981bc3facbb39f87c927d5769bb5f
good-day12 <106358554+good-day12@users.noreply.github.com>
2024-02-21 12:41:47 -0800

    Trying to fix css issues, renamed basePath back to before

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
      *
      * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
      */
-    basePath: "/https://github.com/good-day12/good-day12.github.io", ///good-day12.github.io
+    basePath: "/good-day12.github.io", ///good-day12.github.io   /https://github.com/good-day12/good-day12.github.io
   
     /**
      * Disable server-based image optimization. Next.js does not support
