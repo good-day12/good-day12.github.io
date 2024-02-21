3abf84a89c9ac8adfb4483787f00f99bf5316af7
good-day12 <106358554+good-day12@users.noreply.github.com>
2024-02-21 12:44:23 -0800

    Trying to fix css issues, chagned home link

--- src/app/sideNav.js
+++ src/app/sideNav.js
@@ -12,7 +12,7 @@ export default function SideNav() {
           width={500}
           height={500}
           />
-          <a className= {styles.sideNavLinks} href = "/good-day12.github.io/">Home</a>
+          <a className= {styles.sideNavLinks} href = "/">Home</a>
           <a className= {styles.sideNavLinks} href = "/good-day12.github.io/aboutMe">About Me</a>
           <a className= {styles.sideNavLinks} href = "/good-day12.github.io/aboutMe">USMC Experience</a>
           <a className= {styles.sideNavLinks} href = "/good-day12.github.io/aboutMe">Projects</a>
